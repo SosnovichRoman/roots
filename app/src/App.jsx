5603c1cd73726de1ebc8ef71e8ea63b5e27b5eb4
SosnovichRoman <sosnovichroman@gmail.com>
2023-04-04 17:06:44 +0300

    mixin added

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,6 +2,7 @@ import Footer from './containers/Footer/Footer'
 import Header from './containers/Header/Header'
 import Home from './containers/Home/Home';
 import { Routes, Route, useNavigate } from 'react-router-dom';
+import './common.scss'
 
 function App() {
 
